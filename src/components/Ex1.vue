7f8414afb27fbe0dca659e12ceff34623b84d804
cookiemo729 <58022041+cookiemo729@users.noreply.github.com>
2026-08-14 14:56:53 +0800

    Added Ex1 solution

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -3,7 +3,11 @@
 
     // Add code here
     // challenge: use array  
-    
+    const message = ref('Choose your favorite fruit:')
+    const fruit1 = ref('apple')
+    const fruit2 = ref('orange')
+    const fruit3 = ref('peach')
+
 </script>
 
 <template>
@@ -12,7 +16,7 @@
     <label for=""> <input type="radio" name="fruit"> {{ fruit1 }} </label> 
     <label for=""> <input type="radio" name="fruit"> {{ fruit2 }} </label> 
     <label for=""><input type="radio" name="fruit"> {{ fruit3 }} </label> 
-    
+
 </template>
 
 <style scoped>
